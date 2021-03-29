15e74ec8953880c45aff6480be02d56ed9af97cb
solismc <manolocsolis@gmail.com>
2021-03-29 14:55:40 -0400

    Added HomePage JS file header

---
+++ src/Components/HomePage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import {Link} from "react-router-dom"
+// import "./App.css";
+
+class HomePage extends Component {
+  render() {
+    return (
