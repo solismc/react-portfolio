a473b8b4fc1edfd126ec883258de75a835784377
solismc <manolocsolis@gmail.com>
2021-03-29 09:06:56 -0400

    Removed end of commented out of react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";*/
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
