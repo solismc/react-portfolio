09217f4d15f88ab6ac06e4f1b3ba386d2dd44c3a
solismc <manolocsolis@gmail.com>
2021-03-29 17:04:33 -0400

    Added beautify configurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {Router, Link, Switch, Route } from "react-router-dom";
+/*import {Router, Link, Switch, Route } from "react-router-dom";*/
 import React, { Component } from "react";
 import HomePage from "./components/HomePage";
 import Instructions from "./components/Instructions.js";
@@ -15,8 +15,8 @@ class App extends Component {
     console.log(process.env)
     return (
       <Router history={history}>
-    <div className="App">
-        <nav class="navbar navbar-expand-sm bg-light">
+        <div className="App">
+          <nav class="navbar navbar-expand-sm bg-light">
           <ul className="ul-list">
             <li>
               <Link to="/"></Link>
@@ -37,45 +37,8 @@ class App extends Component {
                 <Link to="/contact"></Link>
             </li>
           </ul>
-        </nav>
-
-    <section>
-    <section>
-    <section class="hero-unit">
-      <section class="bubble-box">
-        <h1 class="text-center">Manuel Solis PSM, PSPO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
-        <h2 class="text-center">Project Manager&nbsp;&nbsp;&nbsp;&nbsp;</h2>
-      <img class="headshot" src="images/Manuel-Headshot.jpeg" alt="Manuel Solis" width="200" height="200"/>
-      <section>
-      <blockquote class="blockquote text-muted text-right">
-        <h2>&nbsp;</h2>
-        <h3> "If you think you can or you can't, either way you are right"</h3>
-        <cite title="Henry Ford">Henry Ford</cite>
-        </blockquote>
-        </section>
-    </section>
-    </section>
-    </section>
-    </section>
-    <section class="section-projects">
-      <section class="section-projects-images">
-      <a class="button" href="/brand.html">
-            <img class="square" src="images/branding.jpg" alt="Branding" height="140" width="auto"/>
-          </a>
-          <a href="/projects.html">
-            <img class="square" src="images/Innovation.png" alt="leadership" height="150" width= "auto"/>
-          </a>
-          <a href="/projects.html">
-            <img class="square" src="images/software-developer-code.jpg" alt="Software-code" height="140" width= "auto"/>
-          </a>
-      </section>
-    </section>
-      <section class="section-header-react">
-      <img src={logo} className="App-logo" alt="logo"/>
-      </section>
-  </div>
-
-  <div>
+          </nav>
+        <div>
             <Switch>
               <Route path="/" exact component={HomePage} />
               <Route path="/brand" exact component={Brand} />
@@ -84,8 +47,9 @@ class App extends Component {
               <Route path="/aboutme" exact component={AboutMe} />
               <Route path="/contact" exact component={Contact} />
             </Switch>
-          </div>
-  </Router>          
+        </div>
+      </div>
+    </Router>          
   );
 }
 
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,7 +6,42 @@ import "./App.css";
 class HomePage extends Component {
   render() {
     return (
-
+<div>        
+<section>
+    <section>
+    <section class="hero-unit">
+      <section class="bubble-box">
+        <h1 class="text-center">Manuel Solis PSM, PSPO &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+        <h2 class="text-center">Project Manager&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+      <img class="headshot" src="images/Manuel-Headshot.jpeg" alt="Manuel Solis" width="200" height="200"/>
+      <section>
+      <blockquote class="blockquote text-muted text-right">
+        <h2>&nbsp;</h2>
+        <h3> "If you think you can or you can't, either way you are right"</h3>
+        <cite title="Henry Ford">Henry Ford</cite>
+        </blockquote>
+        </section>
+    </section>
+    </section>
+    </section>
+    </section>
+    <section class="section-projects">
+      <section class="section-projects-images">
+      <a class="button" href="/brand.html">
+            <img class="square" src="images/branding.jpg" alt="Branding" height="140" width="auto"/>
+          </a>
+          <a href="/projects.html">
+            <img class="square" src="images/Innovation.png" alt="leadership" height="150" width= "auto"/>
+          </a>
+          <a href="/projects.html">
+            <img class="square" src="images/software-developer-code.jpg" alt="Software-code" height="140" width= "auto"/>
+          </a>
+      </section>
+    </section>
+      <section class="section-header-react">
+      <img src={logo} className="App-logo" alt="logo"/>
+      </section>
+  </div>
 
 
         );
